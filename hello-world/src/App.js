07c78deb6d2beaf6e063e8f95eb767b2376f7c86
Pawan Saroj <pawansaroj6783@gmail.com>
2020-09-09 12:52:33 +0530

    hello-world and employee-table projects are created

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const hello = () => (
+    <div>
+        <h1>"Hello World"</h1>
+    </div>
+);
+
+export default hello;
